c8043f28661d12d2b9a0cbbdf65d4f239c23a39d
Portatil <jorgebernabeumira@gmail.com>
2018-11-19 22:12:43 +0100

    inicial
    commit inicial

---
+++ js/blog.js
@@ -0,0 +1,18 @@
+
+
+function cargar_datos_blog() {    
+
+/*<div class="col-12 col-md-6 col-lg-4">
+    <article class="card">
+        <img class="card-img-top" src="img/news/img01.jpg" alt="Article Image">
+        <div class="card-body">
+            <div class="card-subtitle mb-2 text-muted">by <a href="#">John Doe</a> on August 10, 2017</div>
+        <h4 class="card-title"><a href="#" data-toggle="read" data-id="1">Duis aute irure dolor in reprehenderit in voluptate</a></h4>
+        <p class="card-text">Mauris eu eros in metus elementum porta eget sed ligula. Praesent consequat, ipsum molestie pellentesque venenatis.</p>
+        <div class="text-right">						    	
+            <a href="#" class="card-more" data-toggle="read" data-id="1">Read More <i class="ion-ios-arrow-right"></i></a>
+        </div>
+    </div>
+    </article>
+</div>*/
+};
